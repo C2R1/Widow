29279da18a54386305881a0a1354bafb0081d222
AmarOk1412 <amarok@enconn.fr>
2014-10-18 11:55:39 +0200

    maj mod leg

--- arduino/sketch_oct02a/sketch_oct02a.ino
+++ arduino/sketch_oct02a/sketch_oct02a.ino
@@ -1,7 +1,8 @@
 #include <Servo.h>
 
 Servo servo;
-String serial;
+String serial;// = "0101010100101010100101010101010101010101";
+char carlu = 0;
 
 void setup()
 {
@@ -17,12 +18,22 @@ void loop()
   if(serial.length() > 0)
   {
     int v = (int)serial[0] - 48;
-    Serial.println(v);
+    Serial.println(serial);
+    Serial.println(serial.length());
+    Serial.println("-------------");
     servo.write(v*90);
-    delay(500);
+    delay(100);    
+    serial = serial.substring(1);
+    Serial.println(serial);
   }
   else
     servo.write(90);
-  serial = serial.substring(1);
-  Serial.println(serial.length());
+    
+  while(Serial.available() > 0)
+  {
+    carlu = Serial.read();
+    Serial.println("reçu");
+    serial += carlu;
+    delay(100);
+  }
 }
